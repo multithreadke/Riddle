2f78e0f0983ac9b30a2674e7030baa2e5d9b8152
Enrique Multithreadke <multithreadke@gmail.com>
2018-10-09 11:23:11 +0300

    Initial Commit

---
+++ src/com/multi/Main.java
@@ -0,0 +1,14 @@
+package com.multi;
+
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+                Integer[] nums  = {1, 0, 7, 2, 2, 7, 11, 5, 6, 6, 5, 7, 9, 10, 4, 3, 9, 3, 2, 8};
+                Character[] chars = {'a', 'c', 'e', 'i', 'n', 'o', 'p', 'r', 's', 't', 'u','.'};
+                List<Character> charList = ?;
+                System.out.println("@Vodafone.com");
+            }
+    }
+}
